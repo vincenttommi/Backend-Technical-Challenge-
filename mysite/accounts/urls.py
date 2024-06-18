5685ad65daadf2f37d5aff9ce255220bd4bd497e
vincenttommi <vincenttommi@gmail.com>
2024-06-19 00:58:18 +0300

    adding comments

--- mysite/accounts/urls.py
+++ mysite/accounts/urls.py
@@ -5,4 +5,5 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='accounts/signin.html'))
+    # We use auth_views.LoginView.as_view() to specify the view for handling login.
 ]
