3a36a56ed0144982db77098636304fe1827c9cdd
vincenttommi <vincenttommi@gmail.com>
2024-06-19 13:51:28 +0300

    completed designing models

--- mysite/myapi/models.py
+++ mysite/myapi/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    name  = models.CharField(max_length=50)
+    code  = models.CharField(max_length=5)
+    phone_number = models.CharField(max_length=100)
+
+
+
+    def  __str__(self):
+        return  self.name
+    
+
+class Order(models.Model):
+     item = models.CharField(max_length=20)
+     amount  = models.PositiveBigIntegerField(default=0) 
+     time   = models.DateTimeField(auto_now_add=True)
+
+
+     def  __str__(self):
+         return
